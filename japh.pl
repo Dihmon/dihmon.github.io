116a19de95ef0956a518cf8bedb98f8b4c1deee2
Dihmon <dihmon@protonmail.ch>
2019-05-09 18:53:05 +0800

    Added JAPH

---
+++ japh.pl
@@ -0,0 +1,29 @@
+        $i =hex  10 ;
+sub II{ $i-=hex   b }
+sub Il{ $i+=hex   a }
+sub lI{ $i+=hex   1 }
+sub ll{ print$l[$i] }
+
+@l=('q','w','e','r','t','y','u','i','o','P',
+    'a','s','d','f','g','h','J','k','l',';',
+    'z','x','c','v','b','n','m',',','.','/',
+    ' ',' ',' ',' ',' ',' ',' ',' ',' ',    );
+
+ll;
+II;lI;
+ll;Il;II;
+Il;II;Il;II;
+Il;II;Il;II;Il;
+ll;II;lI;lI;lI;lI;
+ll;Il;Il;Il;ll;II;II;
+II;Il;II;Il;ll;Il;lI;lI;
+lI;lI;lI;ll;II;II;lI;lI;lI;
+lI;lI;ll;Il;II;Il;II;Il;II;Il;
+II;ll;Il;lI;ll;II;Il;II;Il;II;ll;
+lI;ll;Il;Il;Il;ll;II;II;II;lI;lI;lI;
+lI;lI;lI;lI;lI;lI;ll;Il;II;Il;II;Il;II;
+Il;II;Il;II;Il;II;Il;II;ll;lI;ll;Il;lI;lI;
+lI;lI;lI;ll;Il;Il;ll;II;II;II;Il;ll;II;Il;II;
+Il;II;Il;II;Il;II;Il;ll;Il;lI;lI;ll;II;lI;lI;lI;
+lI;lI;lI;ll;II;Il;II;Il;II;Il;II;Il;II;ll;lI;ll;Il;
+Il;lI;lI;lI;lI;ll;II;Il;lI;II;Il;lI;II;Il;lI;II;Il;lI;
